b4a0b3ec11c07b2310da95125a2b308d552c00fa
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-03-04 18:10:49 -0300

    definições de variáveis

--- src/com/arthur/primeirasemana/TiposVariaveis.java
+++ src/com/arthur/primeirasemana/TiposVariaveis.java
@@ -8,8 +8,23 @@ public class TiposVariaveis {
 
 
 
-    /* como podemos ver, ao utilizar o valor atribuído ao numeroCurto2, precisamos usar o atributo
+    /* Como podemos ver, ao utilizar o valor atribuído ao numeroCurto2, precisamos usar o atributo
     (short) para indicar que ele é DE FATO um short, mesmo que o valor seja baixo. Isso se deve ao fato de
     que Java é uma linguagem fortemente tipada.
      */
+
+
+
+    // Algumas definições:
+
+    // - Variável = pode ser redefinida.
+
+    String nome = "Arthur";
+
+    /* - Constantes = não podem ser redefinidas depois de declaradas, são representadas com uppercase e
+    e o atributo final */
+
+    final float PI = 3.14F;
+
+    // obs: valores em float precisam terminar com a letra F para indicar que é float e não double.
 }
