db62bfd982557ad85e8a6df0c83935a468a6e986
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-03-12 20:39:09 -0300

    pequenas alterações

--- src/com/arthur/primeirasemana/MinhaClasse.java
+++ src/com/arthur/primeirasemana/MinhaClasse.java
@@ -2,10 +2,10 @@ package com.arthur.primeirasemana;
 
 public class MinhaClasse {
    public static void main (String [] args ) {
-       System.out.print("Hello, World!");
+       System.out.println("Hello, World!");
        final int ANO_2023 = 2023;
 
-       System.out.print(ANO_2023);
+       System.out.println(ANO_2023);
        String primeiroNome = "Arthur";
        String segundoNome = "Gabriel";
        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
